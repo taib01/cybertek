d8ef4c6c014569b81158f26ce3e3310859c93845
taib <taibboudra@gmail.com>
2022-03-11 10:35:01 +0100

    s4-1 adding specification and getting data from Products including Brands and Types

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,14 +15,16 @@ namespace API.Controllers
     public class ProductsController : ControllerBase
     {
 
-        private readonly IProductRepository _repo;
+
+        /*private readonly IProductRepository _repo;
 
         public ProductsController(IProductRepository repo)
         {
             _repo = repo;
 
-        }
-        [HttpGet]
+        }*/
+///////////////////////////////////////////////////////////////////////////
+                /*[HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
             var products = await _repo.GetProductsAsync();
@@ -46,6 +49,51 @@ namespace API.Controllers
         {
            
             return Ok( await _repo.GetProductTypesAsync() );
+        }*/
+//////////////////////////////////////////////////////////////////////////////////////
+
+        private readonly IGenericRepository<Product> _productRepo;
+        private readonly IGenericRepository<ProductBrand> _brandRepo;
+        private readonly IGenericRepository<ProductType> _typeRepo;
+
+        public ProductsController(IGenericRepository<Product> ProductRepo,
+        IGenericRepository<ProductBrand> BrandRepo,
+        IGenericRepository<ProductType> TypeRepo)
+        {
+            _productRepo = ProductRepo;
+            _brandRepo = BrandRepo;
+            _typeRepo = TypeRepo;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification() ; 
+            var products = await _productRepo.ListAsync(spec);
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification(id) ; 
+             return await _productRepo.GetEntityWithSpac(spec) ;
+            
+        }
+        
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {
+            var brands = await _brandRepo.ListAllAsync();
+            return Ok(brands);
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes()
+        {
+           
+            return Ok( await _typeRepo.ListAllAsync() );
         }
     }
 }
---
+++ Core/Specifications/BaseSpecificaton.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public class BaseSpecificaton<T> : ISpecification<T>
+    {
+        public BaseSpecificaton()
+        {
+        }
+
+        public BaseSpecificaton(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+            
+        }
+
+        public Expression<Func<T, bool>> Criteria {get;}
+
+        public List<Expression<Func<T, object>>> Include {get;} =
+            new List<Expression<Func<T, object>>>() ;  
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Include.Add(includeExpression);
+        }
+
+    }
+
+}
