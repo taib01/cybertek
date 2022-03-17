5ab4850d918bde5ecd2ede6e7ed19efbf713e58b
taib-boudra <taibboudra@gmail.com>
2022-03-17 16:29:35 +0100

    s6-5 adding search by name

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -7,6 +7,8 @@ namespace Core.Specifications
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecificaton<Product>
     {
         public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams):base(x=>
+                    ( string.IsNullOrEmpty(productParams.Search) ||x.Name.ToLower()
+                    .Contains(productParams.Search)) && 
                     ( !productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId  ) && 
                     ( !productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId  ) 
                 )
