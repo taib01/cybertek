18c25ce1d6cab9c191cb0ae2574fd118e353257e
taib-boudra <taibboudra@gmail.com>
2022-03-25 11:44:34 +0100

    s8 display data from api with angular

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,14 @@
+
+export interface IProduct {
+    id: number;
+    reference: string;
+    name: string;
+    description: string;
+    price: number;
+    quantity: number;
+    pictureUrl: string;
+    productType: string;
+    productBrand: string;
+}
+
+
