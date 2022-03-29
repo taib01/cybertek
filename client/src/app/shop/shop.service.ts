48cffc1ffa9df132f994b05fc8b329c020dddd14
taib-boudra <taibboudra@gmail.com>
2022-03-29 16:38:31 +0100

    9-7 adding functionalitie of sorting

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -16,7 +16,7 @@ export class ShopService {
 
   constructor( private http: HttpClient) { }
 
-  getProducts(brandId?: number, typeId?:number){
+  getProducts(brandId?: number, typeId?:number , sort?:string){
     let params = new HttpParams();
 
     if(brandId){
@@ -25,6 +25,10 @@ export class ShopService {
 
     if(typeId){
       params = params.append('typeId',typeId.toString());
+    }
+
+    if(sort){
+      params = params.append('sort',sort);
     }
     return this.http.get<IPagination>(this.baseUrlAll, {observe:'response',params})
       .pipe(
