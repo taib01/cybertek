c0d911372f1235ba9bcf005b33c8e15b7fbe3021
taib-boudra <taibboudra@gmail.com>
2022-04-05 07:38:58 +0100

    10-2 adding lazy loading (modification our routes)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
+//import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 
 
@@ -21,7 +21,7 @@ import { HomeModule } from './home/home.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
+    //ShopModule,
     HomeModule
   ],
   providers: [],
