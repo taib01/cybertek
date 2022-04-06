60b169ca17022b895305a90b267c0856e1a0f4da
taib-boudra <taibboudra@gmail.com>
2022-04-06 07:06:51 +0100

    11-3 handling validation errors

--- client/src/app/core/test-error/test-error.component.ts
+++ client/src/app/core/test-error/test-error.component.ts
@@ -9,6 +9,8 @@ import { environment } from 'src/environments/environment';
 })
 export class TestErrorComponent implements OnInit {
   baseUrl = environment.apiUrl;
+  validationErrors : any ;
+
 
   constructor(private http : HttpClient) { }
 
@@ -44,6 +46,7 @@ export class TestErrorComponent implements OnInit {
       console.log(response);
     },error => { 
       console.log(error);
+      this.validationErrors= error.errors
     });
   }
 
