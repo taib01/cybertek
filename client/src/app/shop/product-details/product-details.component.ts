49d8ecc671bbf1e4ab89f377b093d513f69c53eb
taib-boudra <taibboudra@gmail.com>
2022-04-13 08:16:33 +0100

    14-7 adding functionality of adding & increment or decrement items to page product-details

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 
@@ -10,12 +11,27 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product : IProduct ; 
+  quantity = 1 ;
 
-  constructor(private shopService : ShopService , private activetedRoute : ActivatedRoute) { }
+  constructor(private shopService : ShopService , 
+              private activetedRoute : ActivatedRoute,
+              private basketService: BasketService) { }
 
   ngOnInit() {
     this.loadProduct();
   }
+  addItemToBasket(){
+    this.basketService.addItemToBasket(this.product,this.quantity);
+  }
+  incrementQuantity(){
+    this.quantity++;
+  }
+  decrementQuantity(){
+    if (this.quantity >1)
+    this.quantity--;
+  }
+
+
   loadProduct(){
     this.shopService.getProduct(+this.activetedRoute.snapshot.paramMap.get('id')).subscribe(product =>{
       this.product = product;
