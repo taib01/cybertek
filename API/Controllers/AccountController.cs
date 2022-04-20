b0b6018f2b92e8d22d4923df56a448514e1382b2
taib-boudra <taibboudra@gmail.com>
2022-04-20 07:20:17 +0100

    16- adding validation for model of register and adress && change some errors msgs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -97,6 +97,11 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if(CheckEmailExistAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors =new []
+                {"Email Adress is in use"}});
+            }
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
