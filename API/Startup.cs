c73a5fc13276c3222658047796f392b03e034614
taib-boudra <taibboudra@gmail.com>
2022-04-20 07:38:07 +0100

    16-2 Updating some config in swagger for testing identity

--- API/Startup.cs
+++ API/Startup.cs
@@ -91,6 +91,25 @@ namespace API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                var securitySchema =new OpenApiSecurityScheme
+                {
+                    Description="JWT Auth Bearer Scheme",
+                    Name="Authorization",
+                    In=ParameterLocation.Header,
+                    Type=SecuritySchemeType.Http,
+                    Scheme="bearer",
+                    Reference=new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer",securitySchema);
+                var securityRequirment = new OpenApiSecurityRequirement{{securitySchema, new[]
+                {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirment);
+
+
             });
 
             services.AddCors(opt =>
